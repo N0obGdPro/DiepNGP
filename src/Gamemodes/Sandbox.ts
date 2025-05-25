d7bbcafe36417ac2b0359e65a8c26ac0cda6c4e9
NoobGdPro <141958415+N0obGdPro@users.noreply.github.com>
2025-05-25 16:44:20 +0200

    Update Sandbox.ts

--- src/Gamemodes/Sandbox.ts
+++ src/Gamemodes/Sandbox.ts
@@ -26,7 +26,7 @@ import { ArenaFlags } from "../Const/Enums";
  * Manage shape count
 */
 export class SandboxShapeManager extends ShapeManager {
-    
+    /*
     protected get wantedShapes() {
         let i = 0;
         for (const client of this.game.clients) {
@@ -34,7 +34,7 @@ export class SandboxShapeManager extends ShapeManager {
         }
         return Math.floor(i * 30);
     }
-        
+       */ 
 }
 
 /**
