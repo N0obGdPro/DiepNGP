f6981b9bc6d9eb476f0cafe2eb99670cd338b8d8
NoobGdPro <141958415+N0obGdPro@users.noreply.github.com>
2025-05-25 16:03:08 +0200

    Update Sandbox.ts

--- src/Gamemodes/Sandbox.ts
+++ src/Gamemodes/Sandbox.ts
@@ -16,48 +16,160 @@
     along with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
-import GameServer from "../Game";
-import ArenaEntity from "../Native/Arena";
+import * as fs from "fs";
+import { App, SHARED_COMPRESSOR, WebSocket, WebSocketBehavior } from "uWebSockets.js";
+import Client, { ClientWrapper } from "./Client";
+import * as config from "./config"
+import * as util from "./util";
+import GameServer from "./Game";
+import TankDefinitions from "./Const/TankDefinitions";
+import { commandDefinitions } from "./Const/Commands";
+import { ColorsHexCode } from "./Const/Enums";
 
-import ShapeManager from "../Entity/Shape/Manager";
-import { ArenaFlags } from "../Const/Enums";
+const PORT = config.serverPort;
+const ENABLE_API = config.enableApi && config.apiLocation;
+const ENABLE_CLIENT = config.enableClient && config.clientLocation && fs.existsSync(config.clientLocation);
 
-/**
- * Manage shape count
-*/
-export class SandboxShapeManager extends ShapeManager {
-    /*
-    protected get wantedShapes() {
-        let i = 0;
-        for (const client of this.game.clients) {
-            if (client.camera) i += 1;
+if (ENABLE_API) util.log(`Rest API hosting is enabled and is now being hosted at /${config.apiLocation}`);
+if (ENABLE_CLIENT) util.log(`Client hosting is enabled and is now being hosted from ${config.clientLocation}`);
+
+export const bannedClients = new Set<string>();
+const connections = new Map<string, number>();
+const allClients = new Set<Client>();
+const app = App({});
+const games: GameServer[] = [];
+
+app.ws("/*", {
+    compression: SHARED_COMPRESSOR,
+    sendPingsAutomatically: true,
+    maxPayloadLength: config.wssMaxMessageSize,
+    idleTimeout: 10,
+    upgrade: (res, req, context) => {
+        res.upgrade({ client: null, ipAddress: "", gamemode: req.getUrl().slice(1) } as ClientWrapper,
+            req.getHeader('sec-websocket-key'),
+            req.getHeader('sec-websocket-protocol'),
+            req.getHeader('sec-websocket-extensions'),
+            context);
+    },
+    open: (ws: WebSocket<ClientWrapper>) => {
+        const ipAddress = Buffer.from(ws.getRemoteAddressAsText()).toString();
+        let conns = 0;
+        if (connections.has(ipAddress)) conns = connections.get(ipAddress) as number;
+        if (conns >= config.connectionsPerIp || bannedClients.has(ipAddress)) {
+            return ws.close();
         }
-        return Math.floor(i * 12.5);
+        connections.set(ipAddress, conns + 1);
+        const game = games.find(({ gamemode }) => gamemode === ws.getUserData().gamemode);
+        if (!game) {
+            return ws.close();
+        }
+        const client = new Client(ws, game);
+        allClients.add(client);
+        ws.getUserData().ipAddress = ipAddress;
+        ws.getUserData().client = client;
+    },
+    message: (ws: WebSocket<ClientWrapper>, message, isBinary) => {
+        const {client} = ws.getUserData();
+        if (!client) throw new Error("Unexistant client for websocket");
+        client.onMessage(message, isBinary);
+    },
+    close: (ws: WebSocket<ClientWrapper>, code, message) => {
+        const {client, ipAddress} = ws.getUserData();
+        if (client) {
+            connections.set(ipAddress, connections.get(ipAddress) as number - 1);
+            client.onClose(code, message);
+            allClients.delete(client);
+        }
     }
-        */
-}
+} as WebSocketBehavior<ClientWrapper>);
 
-/**
- * Sandbox Gamemode Arena
- */
-export default class SandboxArena extends ArenaEntity {
-    /** Limits shape count to floor(12.5 * player count) */
-	protected shapes: ShapeManager = new SandboxShapeManager(this);
-
-    public constructor(game: GameServer) {
-        super(game);
-
-		this.updateBounds(2500, 2500);
-        this.arenaData.values.flags |= ArenaFlags.canUseCheats;
-		// const w1 = new MazeWall(this.game, 0, 0, 500, 500);
+app.get("/*", (res, req) => {
+    util.saveToVLog("Incoming request to " + req.getUrl());
+    res.onAborted(() => {});
+    if (ENABLE_API && req.getUrl().startsWith(`/${config.apiLocation}`)) {
+        switch (req.getUrl().slice(config.apiLocation.length + 1)) {
+            case "/":
+                res.writeStatus("200 OK").end();
+                return;
+            case "/tanks":
+                res.writeStatus("200 OK").end(JSON.stringify(TankDefinitions));
+                return;
+            case "/servers":
+                res.writeStatus("200 OK").end(JSON.stringify(games.map(({ gamemode, name }) => ({ gamemode, name }))));
+                return;
+            case "/commands":
+                res.writeStatus("200 OK").end(JSON.stringify(config.enableCommands ? Object.values(commandDefinitions) : []));
+                return;
+            case "/colors":
+                res.writeStatus("200 OK").end(JSON.stringify(ColorsHexCode));
+                return;
+        }
     }
 
-    public tick(tick: number) {
-		const arenaSize = Math.floor(25 * Math.sqrt(Math.max(this.game.clients.size, 1))) * 100;
-		if (this.width !== arenaSize || this.height !== arenaSize) this.updateBounds(arenaSize, arenaSize);
+    if (ENABLE_CLIENT) {
+        let file: string | null = null;
+        let contentType = "text/html"
+        switch (req.getUrl()) {
+            case "/":
+                file = config.clientLocation + "/index.html";
+                contentType = "text/html";
+                break;
+            case "/loader.js":
+                file = config.clientLocation + "/loader.js";
+                contentType = "application/javascript";
+                break;
+            case "/input.js":
+                file = config.clientLocation + "/input.js";
+                contentType = "application/javascript";
+                break;
+            case "/dma.js":
+                file = config.clientLocation + "/dma.js";
+                contentType = "application/javascript";
+                break;
+            case "/config.js":
+                file = config.clientLocation + "/config.js";
+                contentType = "application/javascript";
+                break;
+        }
 
-        super.tick(tick);
-    }
-}
+        res.writeHeader("Content-Type", contentType + "; charset=utf-8");
 
+        if (file && fs.existsSync(file)) {
+            res.writeStatus("200 OK").end(fs.readFileSync(file));
+            return;
+        }
 
+        res.writeStatus("404 Not Found").end(fs.readFileSync(config.clientLocation + "/404.html"));
+        return;
+    } 
+});
+
+app.listen(PORT, (success) => {
+    if (!success) throw new Error("Server failed");
+
+    util.log(`Listening on port ${PORT}`);
+
+    // RULES(0): No two game servers should share the same endpoint
+    //
+    // NOTES(0): As of now, both servers run on the same process (and thread) here
+    
+    const sbx = new GameServer("sandbox", "Create Room");
+    const ffa = new GameServer("ffa", "Free For All");
+    const maze = new GameServer("maze", "Maze");
+    const mothers = new GameServer("mot", "2 Motherships");
+    const twoteam = new GameServer("teams", "Two Teams");
+    const fourteam = new GameServer("4teams", "Four Teams");
+    const dominator = new GameServer("dom", "Domination");
+    
+    games.push(sbx, ffa, maze, mothers, twoteam, fourteam, dominator);
+
+    util.saveToLog("Servers up", "All servers booted up.", 0x37F554);
+    util.log("Dumping endpoint -> gamemode routing table");
+    for (const game of games) console.log("> " + `localhost:${config.serverPort}/${game.gamemode}`.padEnd(40, " ") + " -> " + game.name);
+});
+
+process.on("uncaughtException", (error) => {
+    util.saveToLog("Uncaught Exception", '```\n' + error.stack + '\n```', 0xFF0000);
+
+    throw error;
+});
