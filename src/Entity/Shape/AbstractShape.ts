7035b5b9508ded3dd6cc4d2d9f409e1644723dd3
NoobGdPro <141958415+N0obGdPro@users.noreply.github.com>
2025-05-25 14:34:07 +0200

    Update AbstractShape.ts

--- src/Entity/Shape/AbstractShape.ts
+++ src/Entity/Shape/AbstractShape.ts
@@ -20,7 +20,7 @@ import GameServer from "../../Game";
 import LivingEntity from "../Live";
 
 import { Color, PositionFlags, NameFlags } from "../../Const/Enums";
-import { NameGroup } from "../../Native/FieldGroups";
+import { NameGroup, ScoreGroup } from "../../Native/FieldGroups";
 import { AI } from "../AI";
 import { normalizeAngle, PI2 } from "../../util";
 
@@ -41,7 +41,9 @@ export default class AbstractShape extends LivingEntity {
     protected static BASE_ORBIT = 0.005;
     /** The velocity of the shape's orbits. */
     protected static BASE_VELOCITY = 1;
-
+    
+    /** Used to make the scoredata for the as */
+    public scoreData: ScoreGroup = new ScoreGroup(this)
     /** Always existant name field group, present in all shapes. */
     public nameData: NameGroup = new NameGroup(this);
     /** If the shape is shiny or not */
@@ -80,7 +82,17 @@ export default class AbstractShape extends LivingEntity {
         this.isTurning = TURN_TIMEOUT;
     }
 
+    public onKill(killedEntity: LivingEntity) {
+        if (killedEntity.scoreData instanceof ScoreGroup) {
+          this.scoreReward += killedEntity.scoreData.values.score;
+          this.scoreData.score = this.scoreReward;
+        }
+    }
+
     public tick(tick: number) {
+
+        this.scoreData.values.score = this.scoreReward;
+
         if (!this.doIdleRotate) {
             return super.tick(tick);
         }
