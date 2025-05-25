76ff4c90acb19008ae240dde87d4a1f9c243ca34
NoobGdPro <141958415+N0obGdPro@users.noreply.github.com>
2025-05-25 11:12:31 +0200

    Update config.js

--- client/config.js
+++ client/config.js
@@ -21,10 +21,10 @@ const CDN = "https://static.diep.io/";
 const API_URL = `${window.location.href}api/`;
 
 const CHANGELOG = [
-    "Updated recently",
+   "Updated recently",
     "",
-    "Check out the GitHub repository!: github.com/ABCxFF/diepcustom",
-    "Join our Discord server: discord.gg/SyxWdxgHnT :)"
+    "Testing Stuff Diep.io Update Feature of NGP.",
+    "Add Friend Discord of @noobgdpro ! :)"
 ];
 
 const ASM_CONSTS = {
