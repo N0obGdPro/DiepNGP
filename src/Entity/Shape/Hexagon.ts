751e37b9a25d62d5320b648a182d7359f7c4ec99
NoobGdPro <141958415+N0obGdPro@users.noreply.github.com>
2025-05-25 11:48:19 +0200

    Update Hexagon.ts

--- src/Entity/Shape/Hexagon.ts
+++ src/Entity/Shape/Hexagon.ts
@@ -1,30 +1,59 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const AbstractShape_1 = __importDefault(require("./AbstractShape"));
-class Hexagon extends AbstractShape_1.default {
-    constructor(game, isAlpha = false, shiny = (Math.random() < 0.001) && !isAlpha) {
+/*
+    DiepCustom - custom tank game server that shares diep.io's WebSocket protocol
+    Copyright (C) 2022 ABCxFF (github.com/ABCxFF)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
+import GameServer from "../../Game";
+import AbstractShape from "./AbstractShape";
+
+import { Color } from "../../Const/Enums";
+
+/**
+ * Hexagon entity class.
+ */
+export default class Hexagon extends AbstractShape {
+    /** If the Hexagon is an alpha Hexagon or not */
+    public isAlpha: boolean;
+
+    protected static BASE_ROTATION = AbstractShape.BASE_ROTATION / 2;
+    protected static BASE_ORBIT = AbstractShape.BASE_ORBIT / 2;
+    protected static BASE_VELOCITY = AbstractShape.BASE_VELOCITY / 2;
+
+    public constructor(game: GameServer, isAlpha=false, shiny=(Math.random() < 0.001) && !isAlpha) {
         super(game);
-        this.nameData.values.name = isAlpha ? "Alpha Hexagon" : "Hexgon";
+        
+        this.nameData.values.name = isAlpha ? "Alpha Hexagon" : "Hexagon";
+
         this.healthData.values.health = this.healthData.values.maxHealth = (isAlpha ? 10000 : 500);
         this.physicsData.values.size = (isAlpha ? 300 : 100) * Math.SQRT1_2;
         this.physicsData.values.sides = 6;
-        this.styleData.values.color = shiny ? 7 : 19;
+        this.styleData.values.color = shiny ? Color.Shiny : Color.EnemyHexagon;
+
         this.physicsData.values.absorbtionFactor = isAlpha ? 0.05 : 0.5;
         this.physicsData.values.pushFactor = 11;
+
         this.isAlpha = isAlpha;
         this.isShiny = shiny;
+
         this.damagePerTick = isAlpha ? 30 : 18;
-        this.scoreReward = isAlpha ? 10000 : 500;
+        this.scoreReward = isAlpha ? 8000 : 500;
+        
         if (shiny) {
             this.scoreReward *= 100;
             this.healthData.values.health = this.healthData.values.maxHealth *= 10;
         }
     }
 }
-exports.default = Hexagon;
-Hexagon.BASE_ROTATION = AbstractShape_1.default.BASE_ROTATION / 2;
-Hexagon.BASE_ORBIT = AbstractShape_1.default.BASE_ORBIT / 2;
-Hexagon.BASE_VELOCITY = AbstractShape_1.default.BASE_VELOCITY / 2;
